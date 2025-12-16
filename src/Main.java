ac20314f435f9a4f4636540f6bd4ce39409d5b92
joaquinlopezperez <pjoaquinl01@educantabria.es>
2025-12-16 16:18:30 +0100

    Añadir metodo en el main.Java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Mini proyecto Git iniciado");
+        saludar();
+    }
+
+    public static void saludar() {
+        System.out.println("Hola desde la utilidad Java");
     }
 }
